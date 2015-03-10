f2894dec26d099896d55cfbbf598a0251d94dc9a
You-Sheng Yang <vicamo@gmail.com>
2015-03-10 14:46:35 +0800

    Add base emulator product mk files
    Change-Id: Idcafa0be2f3026b0877ee83d029185860dd357c5

---
+++ emulator.mk
@@ -0,0 +1,24 @@
+# Copyright (C) 2015 You-Sheng Yang
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/amphibia/common/core.mk)
+
+include $(SRC_TARGET_DIR)/product/emulator.mk
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.adb.qemud=1
+
+PRODUCT_COPY_FILES += \
+    device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
+    device/generic/goldfish/camera/media_codecs.xml:system/etc/media_codecs.xml
